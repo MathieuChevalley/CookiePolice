5f29f37b6041fdd36c8b37ad0d63d67f0ea15d6e
Mathieu <mathieu.chevalley@epfl.ch>
2018-10-15 12:24:09 -0400

    box displays source code in mhtml

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
-chrome.runtime.onInstalled.addListener(function() {
-    
+function show_content(content) {
+    var reader = new FileReader();
+    reader.addEventListener("loadend", function() {
+        alert(reader.result);
+    });
+    reader.readAsBinaryString(content);
+}
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log("on click listener");
+    chrome.pageCapture.saveAsMHTML({tabId: tab.id}, show_content)
 });
