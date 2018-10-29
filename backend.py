2ca0bb559a82a7e4b873a890e82e151c3cf0c6cf
Matius Chairani <matiuschairani@Matiuss-MBP.wv.cc.cmu.edu>
2018-10-29 15:50:00 -0400

    Updated IP address.

--- backend.py
+++ backend.py
@@ -21,13 +21,18 @@ def form():
 def analyse(link):
     output = open('output.txt', 'w')
 
-    analyticsPattern = r'google-analytics|' \
-                       r'adobe-targeting'
-    browserReplayPattern = r'clicktale.net|' \
-                           r'hotjar.com|' \
-                           r'heapanalytics.com|' \
-                           r'fullstory.com|' \
-                           r'sessioncam.com'
+    analyticsPattern = r'(google-analytics)|' \
+                       r'(adobe-targeting)'
+    xSitePattern = r'(googletagmanager\.com)|' \
+                   r'(doubleclick\.net)'
+    xPlatformPattern = r'(hotjar\.com)|' \
+                       r'(clicktale\.net)'
+    browserReplayPattern = r'(clicktale\.net)|' \
+                           r'(hotjar\.com)|' \
+                           r'(heapanalytics\.com)|' \
+                           r'(fullstory\.com)|' \
+                           r'(sessioncam\.com)|' \
+                           r'(inspectlet\.com)'
 
     req = 'String is empty.'
     res = 'Link is invalid.'
@@ -40,24 +45,68 @@ def analyse(link):
         # BASIC ANALYTICS
         print("Searching for basic analytics services..")
         try:
+            analyticsDict = {"SYS": 1}
             analyticsPatternMatch = re.finditer(analyticsPattern, req)
             if analyticsPatternMatch:
                 res = "<h4>This website uses the following basic analytics service:</h4>"
                 for match in analyticsPatternMatch:
-                    res = res + "<li>" + match.group() + "</li></br>"
-                    print(match.group())
+                    if match.group() in analyticsDict:
+                        print(match.group() + " previously caught.")
+                    else:
+                        print(match.group())
+                        res = res + "<li>" + match.group() + "</li></br>"
+                    analyticsDict[match.group()] = 1
+        except Exception:
+            traceback.print_exc()
+
+        # X SITE TRACKING
+        print("Searching for cross site tracking services..")
+        try:
+            xSiteDict = {"SYS": 1}
+            xSitePatternMatch = re.finditer(xSitePattern, req)
+            if xSitePatternMatch:
+                res = res + "<h4>This website uses the following cross site tracking service:</h4>"
+                for match in xSitePatternMatch:
+                    if match.group() in xSiteDict:
+                        print(match.group() + " previously caught.")
+                    else:
+                        print(match.group())
+                        res = res + "<li>" + match.group() + "</li></br>"
+                    xSiteDict[match.group()] = 1
+        except Exception:
+            traceback.print_exc()
+
+        # X PLATFORM TRACKING
+        print("Searching for cross-platform tracking services..")
+        try:
+            xPlatformDict = {"SYS": 1}
+            xPlatformPatternMatch = re.finditer(xPlatformPattern, req)
+            if xPlatformPatternMatch:
+                res = res + "<h4>This website uses the following cross platform tracking service:</h4>"
+                for match in xPlatformPatternMatch:
+                    if match.group() in xPlatformDict:
+                        print(match.group() + " previously caught.")
+                    else:
+                        print(match.group())
+                        res = res + "<li>" + match.group() + "</li></br>"
+                    xPlatformDict[match.group()] = 1
         except Exception:
             traceback.print_exc()
 
         # BASIC ANALYTICS
         print("Searching for session replay capabilities..")
         try:
-            analyticsPatternMatch = re.finditer(browserReplayPattern, req)
-            if analyticsPatternMatch:
-                res = res + "<h4>This website uses the following session replay service:</h4>"
-                for match in analyticsPatternMatch:
-                    res = res + "<li>" + match.group() + "</li></br>"
-                    print(match.group())
+            sesDict = {"SYS": 1}
+            sesPatternMatch = re.finditer(browserReplayPattern, req)
+            if sesPatternMatch:
+                res = res + "<h4>This website uses the following interaction tracking service:</h4>"
+                for match in sesPatternMatch:
+                    if match.group() in sesDict:
+                        print(match.group() + " previously caught.")
+                    else:
+                        print(match.group())
+                        res = res + "<li>" + match.group() + "</li></br>"
+                    sesDict[match.group()] = 1
         except Exception:
             traceback.print_exc()
 
