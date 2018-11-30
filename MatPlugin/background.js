ba0ca06825e81520d1b16b93af2dc6bd9541179f
Matius Chairani <matiuschairani@Matiuss-MacBook-Pro.local>
2018-11-29 19:44:55 -0500

    Fixed web reload issues, implemented basic logic.

--- MatPlugin/background.js
+++ MatPlugin/background.js
@@ -1,16 +1,22 @@
-var TRACKERLIST = "tracker.csv";
+const TRACKER_LIST_FILE = "trackerList.json";
+const SAME_COMPANY_MULTIPLIER = 1;
+const ANTI_REQUISITE_MULTIPLIER = 1.5;
+const GOOD_BOUNDARY = 15;
+const BAD_BOUNDARY = 50;
+const GOOD_COLOR = "#32CD32";
+const OKAY_COLOR = "#FF7F50";
+const BAD_COLOR = "#000000";
+
+const trackerListUrl = chrome.runtime.getURL(TRACKER_LIST_FILE);
+
+var trackerList = [];
+var trackerTypeList = [];
 
 // Message to send to popup.js
-var string = "URLS:\n\n";
+var string = "";
 
 // Tracker dictionary
-var advertising = new Object();
-var analytics = new Object();
-var customer = new Object();
-var social = new Object();
-var player = new Object();
-var adult = new Object();
-var comments = new Object();
+var allTrackers = new Object();
 
 // Dictionary
 var trackerDict = new Object();
@@ -18,76 +24,271 @@ var companyDict = new Object();
 
 // Score
 var totalScore = 0;
+var trackerScore = 0;
+var pageUrl = "https://...";
+
 var ADVERTISING_SCORE = 3;
 var ANALYTICS_SCORE = 7;
 var CUSTOMER_SCORE = 1;
-var SOCIAL_SCORE = 0;
-var PLAYER_SCORE = 0;
-var ADULT_SCORE = 0;
-var COMMENTS_SCORE = 0;
+var SOCIAL_SCORE = 2;
+var AV_SCORE = 1;
+var ADULT_SCORE = 5;
+var COMMENTS_SCORE = 2;
+var ESSENTIAL_SCORE = 2;
 
-// Temporary TrackerList
-advertising = {"omtrdc.net":"Adobe", "ad.doubleclick.net":"Google", "static.criteo.net":"Criteo", "bat.bing.com":"Bing"};
-analytics = {"ws.sessioncam.com":"SessionCam"};
-customer = {"dp.shoprunner.com":"Shoprunner"};
-social = {};
-player = {};
-adult = {};
-comments = {};
+importTrackers(trackerListUrl);
 
+// Listens for page reloads or change of url in the implicated tab
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url || changeInfo.status === "loading") {
+        string = "";
+        trackerDict = new Object;
+        companyDict = new Object;
+        trackerList = [];
+        trackerTypeList = [];
+        totalScore = 0;
 
-// Intercepts all outgoing HTTP requests and returns it as a string
-chrome.webRequest.onCompleted.addListener(
+        pageUrl = tab.url;
+    }
+
+    chrome.extension.onConnect.addListener(function (message) {
+        message.postMessage(string);
+    })
+});
+
+// Intercepts all outgoing HTTP requests made in Chrome
+chrome.webRequest.onSendHeaders.addListener(
     function(details) {
         var thisUrl = details.url;
         var thisTracker = regexChecker(thisUrl);
+
         if (thisTracker != null) {
-            string = string + totalScore + "\: " + thisTracker + "\n";
+            if (!(trackerList.includes(thisTracker.trackerName))) {
+                trackerList.push(thisTracker.trackerName + " (" + trackerScore + ")");
+            }
+        }
+
+        chrome.browserAction.setBadgeText({text: totalScore.toString()});
+
+        if (totalScore <= GOOD_BOUNDARY) {
+            chrome.browserAction.setBadgeBackgroundColor({color: GOOD_COLOR});
+        } else if (totalScore > BAD_BOUNDARY) {
+            chrome.browserAction.setBadgeBackgroundColor({color: BAD_COLOR});
+        } else {
+            chrome.browserAction.setBadgeBackgroundColor({color: OKAY_COLOR});
         }
     },
     {urls: ["<all_urls>"]},
-    ["responseHeaders"]
+    ["requestHeaders"]
 );
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.extension.onConnect.addListener(function(port) {
-        port.postMessage(string);
-    });
-});
-
 function regexChecker(url) {
-
-    // ORDER THIS IN ORDER OF SEVERITY
-
-    for (const [key, value] of Object.entries(advertising)) {
+    for (const [key, value] of Object.entries(allTrackers)) {
         if (url.includes(key)) {
-            if (!(key in trackerDict)) {
-                totalScore += ADVERTISING_SCORE;
-                trackerDict[key] = value;
-                return value + " Advertising";
+            // Advertising
+            if (value.trackerType == "ad") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = ADVERTISING_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+
+                    if (!(trackerTypeList.includes("advertising"))) {
+                        trackerTypeList.push("advertising");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
             }
-        }
-    }
 
-    for (const [key, value] of Object.entries(analytics)) {
-        if (url.includes(key)) {
-            if (!(key in trackerDict)) {
-                totalScore += ANALYTICS_SCORE;
-                trackerDict[key] = value;
-                return value + " Analytics";
+            // Analytics
+            if (value.trackerType == "an") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = ANALYTICS_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+
+                    if (!(trackerTypeList.includes("analytics"))) {
+                        trackerTypeList.push("analytics");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
             }
-        }
-    }
 
-    for (const [key, value] of Object.entries(customer)) {
-        if (url.includes(key)) {
-            if (!(key in trackerDict)) {
-                totalScore += CUSTOMER_SCORE;
-                trackerDict[key] = value;
-                return value + " Customer Interaction";
+            // Customer Service
+            if (value.trackerType == "cu") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = CUSTOMER_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+                    if (!(trackerTypeList.includes("customer service"))) {
+                        trackerTypeList.push("customer service");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
+            }
+
+            // Adult
+            if (value.trackerType == "xx") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = ADULT_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+
+                    if (!(trackerTypeList.includes("adult websites"))) {
+                        trackerTypeList.push("adult websites");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
+            }
+
+            // Essentials
+            if (value.trackerType == "es") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = ESSENTIAL_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+
+                    if (!(trackerTypeList.includes("essentials"))) {
+                        trackerTypeList.push("essentials");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
+            }
+
+            // Social Media
+            if (value.trackerType == "so") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = SOCIAL_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+
+                    if (!(trackerTypeList.includes("social media"))) {
+                        trackerTypeList.push("social media");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
+            }
+
+            // AVP
+            if (value.trackerType == "av") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = AV_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+
+                    if (!(trackerTypeList.includes("audio & video player"))) {
+                        trackerTypeList.push("audio & video player");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
+            }
+
+            // Comments
+            if (value.trackerType == "co") {
+                if (!(value.trackerName in trackerDict)) {
+                    trackerScore = COMMENTS_SCORE;
+
+                    if (!(value.trackerParent in companyDict)) {
+                        companyDict[value.trackerParent] = 1;
+                    } else {
+                        companyDict[value.trackerParent] += 1;
+                        trackerScore = trackerScore - 1;
+                    }
+
+                    totalScore += trackerScore;
+                    if (!(trackerTypeList.includes("comments"))) {
+                        trackerTypeList.push("comments");
+                    }
+
+                    trackerDict[value.trackerName] = 1;
+
+                    return value;
+                }
             }
         }
     }
 
     return null;
+}
+
+// Function to import tracker list
+function importTrackers(url) {
+    fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            for (var i = 0; i < json.List.length; i++) {
+                allTrackers[json.List[i].trackerPattern] = json.List[i];
+            }
+        });
 }
--- MatPlugin/popup.js
+++ MatPlugin/popup.js
@@ -1,13 +1,63 @@
+var background = chrome.extension.getBackgroundPage();
 
-/*chrome.runtime.onMessage.addListener(function(request, sender, response) {
-    alert("message");
-    document.getElementById("textBox").innerText = request.result;
+const GOOD_BOUNDARY = 15;
+const BAD_BOUNDARY = 50;
+const GOOD_COLOR = "limeGreen";
+const OKAY_COLOR = "coral";
+const BAD_COLOR = "crimson";
+const HEADER_COLOR = "peru";
 
-});*/
-var port = chrome.extension.connect({
-    name: "Sample Communication"
+var message = chrome.extension.connect({
+    name: "Message port from background.js"
 });
 
-port.onMessage.addListener(function(msg) {
-    document.getElementById("textBox").innerText = msg;
+message.onMessage.addListener(function(msg) {
+    if (background.totalScore <= GOOD_BOUNDARY) {
+        document.getElementById("scoreBox").style.color = GOOD_COLOR
+    } else if (background.totalScore > BAD_BOUNDARY) {
+        document.getElementById("scoreBox").style.color = BAD_COLOR;
+    } else {
+        document.getElementById("scoreBox").style.color = OKAY_COLOR;
+    }
+
+    document.getElementById("scoreBox").innerText = background.totalScore;
+    document.getElementById("urlBox").innerText = background.pageUrl;
+
+    var listOfTrackers = background.trackerList.sort();
+    var trackers = "";
+    for (var i = 0; i < listOfTrackers.length; i++) {
+        trackers = trackers + listOfTrackers[i].toUpperCase() + "\n" ;
+    }
+
+    var listOfCompanies = background.companyDict;
+    var listOfCompaniesList = [];
+    var companies = "";
+    for (const [key, value] of Object.entries(listOfCompanies)) {
+        listOfCompaniesList.push(value + " " + key.toUpperCase());
+    }
+
+    listOfCompaniesList = listOfCompaniesList.sort().reverse();
+
+    for (var i = 0; i < listOfCompaniesList.length; i++) {
+        companies = companies + listOfCompaniesList[i] + "\n" ;
+    }
+
+    var listOfTypes = background.trackerTypeList.sort();
+    var types = "";
+
+    for (var i = 0; i < listOfTypes.length; i++) {
+        types = types + listOfTypes[i].toUpperCase() + "\n";
+    }
+
+    document.getElementById("welcomeHeader").style.color = HEADER_COLOR;
+    document.getElementById("typeHeader").style.color = HEADER_COLOR;
+    document.getElementById("companiesHeader").style.color = HEADER_COLOR;
+    document.getElementById("trackerHeader").style.color = HEADER_COLOR;
+
+    document.getElementById("typeHeader").innerText = "Types (" + listOfTypes.length.toString() + ")";
+    document.getElementById("typeBox").innerText = types;
+    document.getElementById("companiesHeader").innerText = "Companies (" + listOfCompaniesList.length.toString() + ")";
+    document.getElementById("companiesBox").innerText = companies;
+    document.getElementById("trackerHeader").innerText = "Trackers (" + listOfTrackers.length.toString() + ")";
+    document.getElementById("trackerBox").innerText = trackers;
 });
